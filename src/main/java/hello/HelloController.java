f40398eb38765c76d72ca1426ff3c8dfed6ea7b5
vfaruk <farukveysel@gmail.com>
2019-09-30 21:46:22 +0300

    scond commit

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Staxter! -- Veysel FARUK";
+        return "Hello from Staxter!";
     }
     
 }
